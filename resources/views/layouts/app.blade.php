6eec770e07fa7f3648fcc264cda12a5d9f7d377e
Assylkhan Myrzaliyev <amyrzali@PMINTL.NET>
2022-02-09 12:47:00 +0600

    added menu links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">{{ __('Contacts') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('favorite') }}">{{ __('Favorites') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
